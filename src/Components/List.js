bf13d3e86b925cec2aaf58ae04e12fbf84856b7a
Alexander Bokedal <alexander.bokedal@gmail.com>
2023-07-23 19:17:01 +0200

    added function to add data to db.json

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { AddButton, ClearButton } from './AddButtons';
+import AddDatabase from './AddDatabase';
 
 function List(props) {
   const { list } = props;
-  
+
   const [display, setDisplay] = useState(list);
-  const [storeOrder, setStoreOrder] = useState([])
+  const [storeOrder, setStoreOrder] = useState([]);
 
   const logList = () => {
-    console.log(list);
-    console.log(storeOrder);
+    console.log(display);
   };
 
   useEffect(() => {
@@ -17,20 +17,47 @@ function List(props) {
       const orderFromServer = await fetchOrder();
       setStoreOrder(orderFromServer);
     };
-  
+
     getOrder();
-  }, []);
-  
+    
+  }, [display]);
+
+  useEffect(() => {
+    updateSortProperty();
+  }, [storeOrder]);
 
   // fetch order
   const fetchOrder = async () => {
-    const res = await fetch('http://localhost:5000/order')
-    const data = await res.json()
+    const res = await fetch('http://localhost:5000/order');
+    const data = await res.json();
+    return data;
+  };
 
-    return data
-  }
+  const handleAddSort = async (updatedItem) => {
+    try {
+      const res = await fetch('http://localhost:5000/order', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedItem)
+      });
 
- 
+      if (!res.ok) {
+        throw new Error('Failed to add the sort value to the database.');
+      }
+
+      const data = await res.json();
+      console.log('Data added:', data);
+
+      // Update the displayed list with the newly added item and its sort property
+      setDisplay((prevDisplay) => [...prevDisplay, data]);
+
+      
+    } catch (error) {
+      console.error('Error adding sort value to the database:', error);
+    }
+  };
 
   const updateSortProperty = () => {
     const customOrder = [
@@ -45,7 +72,7 @@ function List(props) {
       'candy',
       'frozen',
       'drinks',
-      'cleaning',
+      'cleaning'
     ];
 
     const storeOrderMap = {};
@@ -53,7 +80,7 @@ function List(props) {
       storeOrderMap[item.name] = item.sort;
     });
 
-    const sortedList = [...list].sort((a, b) => {
+    const sortedList = [...display].sort((a, b) => {
       const sortA = customOrder.indexOf(storeOrderMap[a.name]);
       const sortB = customOrder.indexOf(storeOrderMap[b.name]);
 
@@ -63,23 +90,36 @@ function List(props) {
       return sortA - sortB;
     });
 
-    setDisplay(sortedList);
+    const updatedSortedList = [];
+    const itemSet = new Set();
+
+    sortedList.forEach((item) => {
+      if (!itemSet.has(item.name)) {
+        updatedSortedList.push({
+          ...item,
+          sort: storeOrderMap[item.name]
+        });
+        itemSet.add(item.name);
+      }
+    });
+
+    setDisplay(updatedSortedList);
   };
 
   return (
     <div className="body">
       <h1>List</h1>
-      <ul className='list-box'>
+      <ul className="list-box">
         {display.map((item, index) => (
           <li key={index}>{item.name}</li>
         ))}
       </ul>
-     
+
       <button className="add-button green" onClick={updateSortProperty}>
-      Sort
-    </button>
-  
-   
+        Sort
+      </button>
+      <button onClick={logList}>Console</button>
+      <AddDatabase handleAddSort={handleAddSort} display={display} updateSortProperty={updateSortProperty} />
     </div>
   );
 }
